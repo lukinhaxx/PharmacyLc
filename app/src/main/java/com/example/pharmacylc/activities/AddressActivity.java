80418bb995ab0a302ba90dc1407bc3ed8381440f
lukinhaxx <126528237+lukinhaxx@users.noreply.github.com>
2023-09-17 20:28:16 -0300

    commit

--- app/src/main/java/com/example/pharmacylc/activities/AddressActivity.java
+++ app/src/main/java/com/example/pharmacylc/activities/AddressActivity.java
@@ -1,6 +1,10 @@
 package com.example.pharmacylc.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,16 +12,73 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.pharmacylc.R;
+import com.example.pharmacylc.adapters.AddressAdapter;
+import com.example.pharmacylc.models.AddressModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
-public class AddressActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressActivity extends AppCompatActivity implements  AddressAdapter.SelectedAddress{
 
     Button addAddress;
+    RecyclerView recyclerView;
+    private List<AddressModel> addressModelList;
+    private AddressAdapter addressAdapter;
+    FirebaseFirestore firestore;
+    FirebaseAuth auth;
+    Button addAddressBtn, paymentBtn;
+    Toolbar toolbar;
+    String mAddress = "";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address);
 
+        toolbar = findViewById(R.id.address_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firestore = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        recyclerView = findViewById(R.id.address_recycler);
         addAddress = findViewById(R.id.add_address_btn);
+        paymentBtn = findViewById(R.id.payment_btn);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        addressModelList = new ArrayList<>();
+        addressAdapter = new AddressAdapter(getApplicationContext(),addressModelList, this);
+        recyclerView.setAdapter(addressAdapter);
+
+        firestore.collection("User").document(auth.getCurrentUser().getUid())
+                        .collection("Address").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(DocumentSnapshot doc : task.getResult().getDocuments()){
+                                AddressModel addressModel = doc.toObject(AddressModel.class);
+                                addressModelList.add(addressModel);
+                                addressAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                });
+
+        paymentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AddressActivity.this,PagamentoActivity.class));
+            }
+        });
+
 
         addAddress.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +86,11 @@ public class AddressActivity extends AppCompatActivity {
                 startActivity(new Intent(AddressActivity.this,AddAddressActivity.class));
             }
         });
+    }
+
+    @Override
+    public void setAddress(String address) {
+        mAddress = address;
+
     }
 }
